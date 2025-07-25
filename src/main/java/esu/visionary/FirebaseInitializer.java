b6104014cc66339448046419bdad3f46218b07f6
rex0812 <0812rex@gmail.com>
2025-07-26 02:11:15 +0900

    💥 Replace all files with new project version

---
+++ src/main/java/esu/visionary/FirebaseInitializer.java
@@ -0,0 +1,34 @@
+package esu.visionary;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+
+@Component
+public class FirebaseInitializer {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            InputStream serviceAccount = getClass()
+                    .getClassLoader()
+                    .getResourceAsStream("firebase/visonary-d749f-firebase-adminsdk-fbsvc-1f8ac692fc.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("✅ Firebase initialized successfully.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("❌ Firebase initialization failed.");
+        }
+    }
+}
---
+++ src/test/java/esu/visionary/VisionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package esu.visionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
